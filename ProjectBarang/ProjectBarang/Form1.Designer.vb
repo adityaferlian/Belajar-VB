bd7828fee02b915ad8f23cf29f416e1a7a832d31
Aditya Ferlian <122790531+adityaferlian@users.noreply.github.com>
2023-01-24 10:12:05 +0700

    Add files via upload

---
+++ ProjectBarang/ProjectBarang/Form1.Designer.vb
@@ -0,0 +1,155 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.display = New System.Windows.Forms.DataGridView
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.del = New System.Windows.Forms.Button
+        Me.upd = New System.Windows.Forms.Button
+        Me.ins = New System.Windows.Forms.Button
+        Me.TextBox1 = New System.Windows.Forms.TextBox
+        Me.TextBox2 = New System.Windows.Forms.TextBox
+        Me.TextBox3 = New System.Windows.Forms.TextBox
+        CType(Me.display, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'display
+        '
+        Me.display.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.display.Location = New System.Drawing.Point(12, 166)
+        Me.display.Name = "display"
+        Me.display.Size = New System.Drawing.Size(354, 151)
+        Me.display.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(17, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(69, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Kode Barang"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(16, 50)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(72, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Nama Barang"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(17, 84)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(73, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Harga Barang"
+        '
+        'del
+        '
+        Me.del.Location = New System.Drawing.Point(289, 119)
+        Me.del.Name = "del"
+        Me.del.Size = New System.Drawing.Size(58, 26)
+        Me.del.TabIndex = 9
+        Me.del.Text = "DELETE"
+        Me.del.UseVisualStyleBackColor = True
+        '
+        'upd
+        '
+        Me.upd.Location = New System.Drawing.Point(201, 119)
+        Me.upd.Name = "upd"
+        Me.upd.Size = New System.Drawing.Size(62, 26)
+        Me.upd.TabIndex = 8
+        Me.upd.Text = "UPDATE"
+        Me.upd.UseVisualStyleBackColor = True
+        '
+        'ins
+        '
+        Me.ins.Location = New System.Drawing.Point(110, 119)
+        Me.ins.Name = "ins"
+        Me.ins.Size = New System.Drawing.Size(58, 26)
+        Me.ins.TabIndex = 7
+        Me.ins.Text = "INSERT"
+        Me.ins.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(109, 15)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(236, 20)
+        Me.TextBox1.TabIndex = 10
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(110, 47)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(235, 20)
+        Me.TextBox2.TabIndex = 11
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(109, 81)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(236, 20)
+        Me.TextBox3.TabIndex = 12
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(378, 321)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.del)
+        Me.Controls.Add(Me.upd)
+        Me.Controls.Add(Me.ins)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.display)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        CType(Me.display, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents display As System.Windows.Forms.DataGridView
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents del As System.Windows.Forms.Button
+    Friend WithEvents upd As System.Windows.Forms.Button
+    Friend WithEvents ins As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+
+End Class
---
+++ ProjectBarang/ProjectBarang/Form1.vb
@@ -0,0 +1,88 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+    Dim Conn As SqlConnection
+    Dim Da As SqlDataAdapter
+    Dim Cmd As SqlCommand
+    Dim Rd As SqlDataReader
+    Dim Ds As DataSet
+    Dim MyDB As String
+    Sub koneksi()
+        MyDB = "data source=NITRO5\SQLEXPRESS;initial catalog=DB_Indomaret;integrated security=true"
+        Conn = New SqlConnection(MyDB)
+        If Conn.State = ConnectionState.Closed Then Conn.Open()
+    End Sub
+
+    Sub kondisiAwal()
+        Call koneksi()
+        Da = New SqlDataAdapter("Select * from TBL_Barang", Conn)
+        Ds = New DataSet
+        Ds.Clear()
+        Da.Fill(Ds, "TBL_Barang")
+        display.DataSource = (Ds.Tables("TBL_Barang"))
+        TextBox1.MaxLength = 10
+        Call clearText()
+
+    End Sub
+    Sub clearText()
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+    End Sub
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Call kondisiAwal()
+
+    End Sub
+
+   
+    Private Sub ins_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ins.Click
+        If TextBox1.Text = "" Or TextBox2.Text = "" Or TextBox3.Text = "" Then
+            MsgBox("Data Belum Lengkap")
+        Else
+            Call koneksi()
+            Dim InputData As String = "insert into TBL_Barang values ('" & TextBox1.Text & "','" & TextBox2.Text & "','" & TextBox3.Text & "')"
+            Cmd = New SqlCommand(InputData, Conn)
+            Cmd.ExecuteNonQuery()
+            MsgBox("Data Terinput")
+            Call kondisiAwal()
+        End If
+    End Sub
+
+    Private Sub upd_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles upd.Click
+        If TextBox1.Text = "" Or TextBox2.Text = "" Or TextBox3.Text = "" Then
+            MsgBox("Data Belum Lengkap")
+        Else
+            Call koneksi()
+            Dim UpdateData As String = "update TBL_Barang set Nama_Barang ='" & TextBox2.Text & "',Harga_Barang='" & TextBox3.Text & "' where Kode_Barang='" & TextBox1.Text & "' "
+            Cmd = New SqlCommand(UpdateData, Conn)
+            Cmd.ExecuteNonQuery()
+            MsgBox("Data Terupdate")
+            Call kondisiAwal()
+        End If
+    End Sub
+
+    Private Sub del_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles del.Click
+        Call koneksi()
+        Dim DeleteData As String = "delete TBL_Barang where Kode_Barang='" & TextBox1.Text & "'"
+        Cmd = New SqlCommand(DeleteData, Conn)
+        Cmd.ExecuteNonQuery()
+        MsgBox("Data Dihapus")
+        Call kondisiAwal()
+    End Sub
+
+    Private Sub TextBox1_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TextBox1.KeyPress
+        If e.KeyChar = Chr(10) Then
+            Call koneksi()
+            Cmd = New SqlCommand("select * from TBL_Barang where Kode_Barang='" & TextBox1.Text & "'", Conn)
+            Rd = Cmd.ExecuteReader
+            Rd.Read()
+            If Rd.HasRows Then
+                TextBox2.Text = Rd.Item("Nama_Barang")
+                TextBox3.Text = Rd.Item("Harga_Barang")
+            Else
+                MsgBox("Data Tidak Ada")
+            End If
+        End If
+    End Sub
+
+End Class
